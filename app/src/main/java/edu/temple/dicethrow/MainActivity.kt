00f88c1b891694f9bac998237ea2a0cf9ebc36dc
Joseph-A-F <149717113+Joseph-A-F@users.noreply.github.com>
2026-03-18 15:46:28 -0400

    initial implementation

--- app/src/main/java/edu/temple/dicethrow/MainActivity.kt
+++ app/src/main/java/edu/temple/dicethrow/MainActivity.kt
@@ -2,10 +2,13 @@ package edu.temple.dicethrow
 
 import android.os.Bundle
 import android.widget.Button
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
+/** Lab Activity Branch
+ * 1. DONE  Create a new branch of your previous in-class activity (Die Roll)
+ * 2. Modify the DieFragment class implementation to accept the number sides the die should have (6 is the default) using a Factory Method
+ * 3. Add an additional FragmentContainerView to the Activity's layout and include another die so that clicking the Roll button results in 2 dice being rolled
+ */
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,14 +16,22 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         if (supportFragmentManager.findFragmentById(R.id
             .fragmentContainerView) == null) {
-            val dieFragment = DieFragment.newInstance(20)
+            val dieFragment = DieFragment.newInstance(6)
             supportFragmentManager.beginTransaction()
                 .add(R.id.fragmentContainerView, dieFragment)
+                .commit()
+            val dieFragment2 = DieFragment.newInstance(6)
+            supportFragmentManager.beginTransaction()
+                .add(R.id.fragmentContainerView2, dieFragment2)
                 .commit()
         }
         findViewById<Button>(R.id.rollDiceButton).setOnClickListener {
             supportFragmentManager
                 .findFragmentById(R.id.fragmentContainerView)?.run{
+                    (this as DieFragment).throwDie()
+                }
+            supportFragmentManager
+                .findFragmentById(R.id.fragmentContainerView2)?.run{
                     (this as DieFragment).throwDie()
                 }
 
